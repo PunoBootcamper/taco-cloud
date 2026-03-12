72a8052941e482090688603c3d67e0305863cd67
William Lupaca Ticona <wlupaca@osiptel.gob.pe>
2026-03-12 14:15:32 -0500

    2.4 Working with view controllers

--- src/test/java/com/example/tacocloud/controllers/HomeControllerTest.java
+++ src/test/java/com/example/tacocloud/controllers/HomeControllerTest.java
@@ -2,7 +2,9 @@ package com.example.tacocloud.controllers;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
@@ -11,7 +13,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-@WebMvcTest(HomeController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class HomeControllerTest {
     @Autowired
     private MockMvc mockMvc;
