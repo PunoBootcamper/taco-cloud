4e0ed00722e940a34ec879e732257955cc93d472
William Lupaca Ticona <wlupacat@gmail.com>
2026-03-13 01:05:10 -0500

    3.2 Working with Spring Data JDBC

--- src/main/java/com/example/tacocloud/domain/Ingredient.java
+++ src/main/java/com/example/tacocloud/domain/Ingredient.java
@@ -1,14 +1,32 @@
 package com.example.tacocloud.domain;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.relational.core.mapping.Table;
 
 @Data
-public class Ingredient {
+@Table
+@AllArgsConstructor
+@NoArgsConstructor(access= AccessLevel.PRIVATE, force=true)
+public class Ingredient implements Persistable<String> {
+    @Id
     private final String id;
     private final String name;
     private final Type type;
 
+
+
     public enum Type{
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
     }
+    @Override
+    public boolean isNew() {
+        return id == null;
+    }
+
 }
--- src/main/java/com/example/tacocloud/domain/TacoOrder.java
+++ src/main/java/com/example/tacocloud/domain/TacoOrder.java
@@ -2,6 +2,8 @@ package com.example.tacocloud.domain;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.CreditCardNumber;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
@@ -12,10 +14,12 @@ import java.util.Date;
 import java.util.List;
 
 @Data
+@Table
 public class TacoOrder implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
     private Long id;
 
     private Date placedAt;
@@ -38,7 +42,7 @@ public class TacoOrder implements Serializable {
     @CreditCardNumber(message="Not a valid credit card number")
     private String ccNumber;
 
-    @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([2-9][0-9])$",
+    @Pattern(regexp= "^(0[1-9]|1[0-2])([/])([2-9][0-9])$",
             message="Must be formatted MM/YY")
     private String ccExpiration;
 
