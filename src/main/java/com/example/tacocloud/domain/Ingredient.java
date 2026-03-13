bda0007f5d4a9296a6c8135befcea41a2009fab9
William Lupaca Ticona <wlupaca@osiptel.gob.pe>
2026-03-13 15:39:02 -0500

    3.3 Persisting data with Spring Data JPA

--- src/main/java/com/example/tacocloud/domain/Ingredient.java
+++ src/main/java/com/example/tacocloud/domain/Ingredient.java
@@ -1,31 +1,28 @@
 package com.example.tacocloud.domain;
 
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.domain.Persistable;
-import org.springframework.data.relational.core.mapping.Table;
+
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.EnumType;
+import javax.persistence.Id;
 
 @Data
-@Table
+@Entity
 @AllArgsConstructor
-@NoArgsConstructor(access= AccessLevel.PRIVATE, force=true)
-public class Ingredient implements Persistable<String> {
+@NoArgsConstructor
+public class Ingredient {
     @Id
     private String id;
     private String name;
+    
+    @Enumerated(EnumType.STRING)
     private Type type;
-
-
 
     public enum Type{
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
-    }
-    @Override
-    public boolean isNew() {
-        return id == null;
     }
 
 }
--- src/main/java/com/example/tacocloud/repository/OrderRepository.java
+++ src/main/java/com/example/tacocloud/repository/OrderRepository.java
@@ -1,8 +1,19 @@
 package com.example.tacocloud.repository;
 
 import com.example.tacocloud.domain.TacoOrder;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface OrderRepository extends CrudRepository<TacoOrder,Long> {
+import java.util.Date;
+import java.util.List;
 
+public interface OrderRepository extends CrudRepository<TacoOrder,Long> {
+    List<TacoOrder> findByDeliveryZip(String deliveryZip);
+    List<TacoOrder> readOrdersByDeliveryZipAndPlacedAtBetween(
+            String deliveryZip, Date startDate, Date endDate);
+    List<TacoOrder> findByDeliveryNameAndDeliveryCityIgnoreCase(
+            String deliveryName, String deliveryCity);
+    List<TacoOrder> findByDeliveryCityOrderByDeliveryName(String city);
+    @Query("SELECT o FROM TacoOrder o WHERE o.deliveryCity='Seattle'")
+    List<TacoOrder> readOrdersDeliveredInSeattle();
 }
